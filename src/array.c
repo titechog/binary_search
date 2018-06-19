5cf8025b2fe6040be5e671a0c5ace9cd5cb6734d
Daisuke Ogawa <ogawa.d.ac@m.titech.ac.jp>
2018-06-19 16:17:23 +0900

    2

--- src/array.c
+++ src/array.c
@@ -4,15 +4,27 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    return (long long int) A[m] >= k;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = n;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", ub);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
   return 0;
 }
