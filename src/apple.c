0f2aa476380a29e2d0744031cb2adf906249d9dd
Daisuke Ogawa <ogawa.d.ac@m.titech.ac.jp>
2018-06-26 11:30:28 +0900

    4

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,48 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i, s, t;
+    t = k;
+    i = 1;
+    while(i < n+1 && t >= 0){
+        if(A[i] < m){
+            t = t - 1;
+            i = i + 1;
+        }
+        else{
+            s = A[i];
+            while(s > 0){
+                s = s - m;
+                t = t - 1;
+        }
+            i = i+1;
+        }
+    }
+    return (int) t >= 0;
+    }
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n , &k);
+    lb = 0;
+    for(i = 1; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    ub = 0;
+    for(i = 1; i < n; i++){
+        ub = ub + A[i];
+    }
+    int l = ub / k;
+    while(ub - lb > 1) {
+            int m = (lb + ub) / 2;
+            if(p(m)){
+                ub = m;
+            }
+            else {
+                lb = m;
+            }
+    }
+    printf("%d\n", ub);
+    return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,39 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int i, s, t;
+    s = 0;
+    for(i = 1; i < n; i++){
+        int t = A[i] / m;
+        s = s + t;
+    }
+    return (int) s < k;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n , &k);
+    lb = 0;
+    for(i = 1; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    ub = 0;
+    for(i = 1; i < n; i++){
+        ub = ub + A[i];
+    }
+    int l = ub / k;
+    ub = l + 1;
+    while(ub - lb > 1) {
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", lb);
+    return 0;
 }
+
